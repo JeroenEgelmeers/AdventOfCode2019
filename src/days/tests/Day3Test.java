b0288ec24519d471ebf5c8e2d202c07037d3a8fa
Jeroen Egelmeers <jeroen.egelmeers@philips.com>
2019-12-09 11:08:44 +0100

    Working version 3.1

--- src/days/tests/Day3Test.java
+++ src/days/tests/Day3Test.java
@@ -7,34 +7,33 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class Day3Test implements DayTestInterface {
   @Override
   @Test
   public void part_1_solution() {
-    WiresPathCalculator pathCalculator = new WiresPathCalculator(22000, 22000, new DayFileReader().fileToStringList(PATH_TO_FILE, "\n"));
-    int[] i = pathCalculator.getClosestMatch();
-    int distance = pathCalculator.getDistanceToCentralPoint(i);
-    System.out.println("Output is: " + i[0] + " " + i[1] + ". Distance: " + distance);
+    WiresPathCalculator pathCalculator = new WiresPathCalculator(new DayFileReader().fileToStringList(PATH_TO_FILE, "\n"));
+    int distance = pathCalculator.getClosestMatch();
+    Assert.assertEquals(1337, distance);
   }
 
   @Test
-  @Override
-  public void part_2_solution() {
+  public void part_1_first_example() {
     List<String> list = new ArrayList<>();
-    String[] w1 = {"R75","D30","R83","U83","L12","D49","R71","U7","L72"};
-    String[] w2 = {"U62","R66","U55","R34","D71","R55","D58","R83"};
-    list.addAll(Arrays.asList(w1));
-    list.addAll(Arrays.asList(w2));
+    String w1 = "R75,D30,R83,U83,L12,D49,R71,U7,L72";
+    String w2 = "U62,R66,U55,R34,D71,R55,D58,R83";
+    list.add(w1);
+    list.add(w2);
 
-    WiresPathCalculator pathCalculator = new WiresPathCalculator(10000, 10000, list);
-    int[] i = pathCalculator.getClosestMatch();
-    int distance = pathCalculator.getDistanceToCentralPoint(i);
+    WiresPathCalculator pathCalculator = new WiresPathCalculator(list);
+    int distance = pathCalculator.getClosestMatch();
     Assert.assertEquals(159, distance);
   }
 
 
-  private static final String PATH_TO_FILE = "../src/days/textFiles/day3.txt";
+  @Test
+  @Override
+  public void part_2_solution() {}
+  private static final String PATH_TO_FILE = "src/days/textFiles/day3.txt";
 }
--- src/models/WiresPathCalculator.java
+++ src/models/WiresPathCalculator.java
@@ -2,41 +2,40 @@ package models;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 public class WiresPathCalculator {
   private String wireOne;
   private String wireTwo;
-  private int centralPointX;
-  private int centralPointY;
+  private int centralPointX = 0;
+  private int centralPointY = 0;
 
-  private String[][] grid;
+  private HashMap<Integer, HashMap<Integer, String>> grid = new HashMap();
   private List<int[]> intersectionPoints;
 
-  public WiresPathCalculator(int gridX, int gridY, List<String> wires) {
+  public WiresPathCalculator(List<String> wires) {
     this.intersectionPoints = new ArrayList<>();
     this.wireOne = wires.get(0).replaceAll("(\\r|\\n)", "");
     this.wireTwo = wires.get(1).replaceAll("(\\r|\\n)", "");
-    this.grid = new String[gridX][gridY];
-    initGrid(gridX, gridY);
-    renderWireInGrid(Arrays.asList(this.wireOne.split(",")));
-    renderWireInGrid(Arrays.asList(this.wireTwo.split(",")));
+    renderWireInGrid(Arrays.asList(this.wireOne.split(",")), "A");
+    renderWireInGrid(Arrays.asList(this.wireTwo.split(",")), "B");
   }
 
-  private void renderWireInGrid(List<String> wire) {
+  private void renderWireInGrid(List<String> wire, String wireName) {
     int[] currentPos = { centralPointX, centralPointY };
     for(String position : wire) {
-      currentPos = wireAction(position, currentPos[0], currentPos[1]);
+      currentPos = wireAction(position, wireName, currentPos);
     }
   }
 
   // TODO: Better name; get closest point from central point where wires hit eachother.
-  public int[] getClosestMatch() {
-    int[] closest = {};
+  public int getClosestMatch() {
+    int closest = Integer.MAX_VALUE;
     for(int[] iPoint : this.intersectionPoints) {
-       if (closest.length == 0) { closest = iPoint; }
-       if (Math.abs(centralPointX+centralPointY - iPoint[0]+iPoint[1]) < Math.abs(centralPointX+centralPointY - closest[0]+closest[1])) {
-         closest = iPoint;
+       int distance = getDistanceToCentralPoint(iPoint);
+       if (distance < closest ) {
+         closest = distance;
        }
     }
 
@@ -44,10 +43,13 @@ public class WiresPathCalculator {
   }
 
   public int getDistanceToCentralPoint(int[] iPoint) {
-    return (iPoint[0] - this.centralPointX) + (iPoint[1] - this.centralPointY);
+    return Math.abs(iPoint[0] - this.centralPointX) + Math.abs(iPoint[1] - this.centralPointY);
   }
 
-  private int[] wireAction(String action, int currentX, int currentY) {
+  private int[] wireAction(String action, String wireName, int[] currentPos) {
+    int currentX = currentPos[0];
+    int currentY = currentPos[1];
+
     int steps = 0;
     try {
        steps = Integer.parseInt(action.substring(1));
@@ -57,62 +59,42 @@ public class WiresPathCalculator {
 
       switch(action.charAt(0)) {
         case 'R':
-          for(int i = currentX + 1; i < currentX+steps-1; i++) { updateGrid(i, currentY); }
-          this.grid[currentX+steps][currentY] = "+";
+          for(int i = currentX + 1; i < currentX + (steps - 1); i++) { updateGrid(i, currentY, wireName); }
+          updateGrid(currentX + steps, currentY, "+");
           return new int[]{currentX + steps, currentY};
         case 'U':
-          for(int i = currentY+steps - 1; i > currentY-1; i--) { updateGrid(currentX, i); }
-          this.grid[currentX][currentY - steps] = "+";
-          return new int[]{currentX, currentY - steps};
+          for(int i = currentY + 1; i < currentY + (steps - 1); i++) { updateGrid(currentX, i, wireName); }
+          updateGrid(currentX, currentY + steps, "+");
+          return new int[]{currentX, currentY + steps};
         case 'L':
-          for(int i = currentX+steps-1; i > currentX-1; i--) { updateGrid(i, currentY); }
-          this.grid[currentX - steps][currentY] = "+";
+          for(int i = currentX - 1; i > currentX - (steps - 1); i--) { updateGrid(i, currentY, wireName); }
+          updateGrid(currentX - steps, currentY, "+");
           return new int[]{currentX - steps, currentY};
         case 'D':
-          for(int i = currentY+1; i < currentY+steps-1; i++) { updateGrid(currentX, i); }
-          this.grid[currentX][currentY + steps] = "+";
-          return new int[]{currentX, currentY + steps};
+          for(int i = currentY - 1; i > currentY  - (steps - 1); i--) { updateGrid(currentX, i, wireName); }
+          updateGrid(currentX, currentY - steps, "+");
+          return new int[]{currentX, currentY - steps};
         default:
           break;
       }
       return new int[]{};
   }
 
-  private void updateGrid(int gridX, int gridY) {
-    if (this.grid[gridX][gridY].equals(".")) {
-      this.grid[gridX][gridY] = "-";
-    }else {
-      this.grid[gridX][gridY] = "X";
+  private void updateGrid(int gridX, int gridY, String wireName) {
+    String current = ".";
+    if (!this.grid.containsKey(gridY)) {
+      this.grid.put(gridY, new HashMap<>());
+    } else {
+      if (this.grid.get(gridY).containsKey(gridX)) {
+        current = this.grid.get(gridY).get(gridX);
+      }
+    }
+
+    if (current.equals(".") ) {
+      this.grid.get(gridY).put(gridX, wireName);
+    } else if(!current.equals(wireName) && !current.equals("o")) {
+      this.grid.get(gridY).put(gridX, "X");
       this.intersectionPoints.add(new int[]{gridX, gridY});
     }
   }
-
-  private void initGrid(int gridX, int gridY) {
-    for (int row = 0; row < gridX; row++)
-    {
-      for (int column = 0; column < gridY; column++) {
-        if (row == (int)(gridX*(70/100.0f)) && column == (int)(gridY*(20/100.0f))) {
-          grid[row][column] = "o";
-          this.centralPointX = row;
-          this.centralPointY = column;
-        }else {
-          grid[row][column] = ".";
-        }
-
-      }
-    }
-  }
-
-  public void printGrid() {
-    for (String[] row : grid)
-    {
-      for (String value : row)
-      {
-        System.out.printf("%2s ", value);
-      }
-      System.out.println("");
-    }
-  }
-
-
 }
